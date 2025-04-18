ab384b48a601f052e65afc00a15fc26622303eb5
muraliinfoservices <Murali.Vissamsetti@infoservices.com>
2025-04-18 21:33:50 +0530

    Update data.js
    Aws services 6 page migration completed

--- data.js
+++ data.js
@@ -131,7 +131,7 @@ const migrationData = {
     teamMembers: [
       { name: "Manjusha", tasks: 14, completed: 4, pending: 10 },
       { name: "Leela", tasks: 20, completed: 8, pending: 12 },
-      { name: "Murali", tasks: 18, completed: 8, pending: 10 },
+      { name: "Murali", tasks: 18, completed: 8+6, pending: 4 },
       { name: "Kishan", tasks: 14, completed: 14, pending: 0 },
       { name: "Tarun", tasks: 39, completed: 0, pending: 39 },
       { name: "Team as they develop", tasks: 22, completed: 0, pending: 22 }
